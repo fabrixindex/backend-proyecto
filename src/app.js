8524c22ebe849b84c15d9a0311db7141b33febf0
fabrixindex <fabrixindex@gmail.com>
2023-05-22 23:46:40 -0300

    todo ok para el tercer entregable

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import ProductManager from "./productManager.js";
 
 const app = express();
-const productM = new ProductManager();
+const productM = new ProductManager("./products.json");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,23 +10,25 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/productos/:id", async (req, res) => {
   try {
     const productId = req.params.id;
-    const producto = await productM.getProducts();
-    const filteredProduct = producto.find((p) => p.code === productId);
+    const producto = await productM.getProduct();
 
-    if (filteredProduct) {
-      res.send(filteredProduct);
-    } else {
-      res.status(404).send({ message: "Producto no encontrado" });
+    if (productId) {
+      const filteredProduct = producto.find((p) => p.id === Number(productId));
+      if (filteredProduct) {
+        res.send(filteredProduct);
+      } else {
+        res.status(404).send({ message: "Producto no encontrado" });
+      }
     }
   } catch (error) {
     res.status(500).send({ message: "Error al obtener los productos" });
   }
-});
+}); //EJEMPLO: http://localhost:8080/productos/3
 
 app.get("/productos", async (req, res) => {
   try {
     const limit = req.query.limit;
-    const productos = await productM.getProducts();
+    const productos = await productM.getProduct();
 
     if (limit && !isNaN(limit)) {
       const limitedProducts = productos.slice(0, Number(limit));
@@ -37,7 +39,7 @@ app.get("/productos", async (req, res) => {
   } catch (error) {
     res.status(500).send({ message: "Error al obtener los productos" });
   }
-});
+}); //EJEMPLO: http://localhost:8080/productos?limit=1
 
 app.listen(8080, () => {
   console.log("Servidor escuchando en el puerto 8080");

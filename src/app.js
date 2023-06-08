df1385829f2db710c9bcfc47b9c7a4b8bb0f4c96
fabrixindex <fabrixindex@gmail.com>
2023-06-08 01:42:10 -0300

    preparando el cuarto desafio entregable

--- src/app.js
+++ src/app.js
@@ -1,15 +1,42 @@
 import express from "express";
 import productsRouter from "../src/routes/productsRouter.js";
 import cartRouter from "./routes/cartRouter.js";
+import viewRouter from "./routes/viewRouter.js";
+import handlebars from "express-handlebars";
+import __dirname from './utils.js';
+import { Server } from "socket.io";
+
+/*------------------------------ CONFIGURACION EXPRESS -------------------------------------*/
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+/*------------------------------- SERVIDOR HTTP --------------------------------------------*/
+
+const httpServer = app.listen(8080, () => console.log("Servidor escuchando en el puerto 8080"));
+
+/*-------------------------------- SOCKET SERVER -------------------------------------------*/
+
+const socketServer = new Server(httpServer)
+
+/*-------------------------------------- ROUTES --------------------------------------------*/
 
 app.use("/api/productos", productsRouter);
 app.use("/api/carts", cartRouter);
 
-app.listen(8080, () => {
-  console.log("Servidor escuchando en el puerto 8080");
+app.use("/", viewRouter);
+/*----------------------------- CONFIGURACION HANDLEBARS -----------------------------------*/
+
+app.engine(`handlebars`, handlebars.engine());
+app.set(`views`, `${__dirname}/views`);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(`${__dirname}/public`))
+
+/*----------------------------- CONFIGURACION SOCKET SERVER---------------------------------*/
+
+socketServer.on("connection", socket => {
+  console.log("Cliente conectado");
 });
+
---
+++ src/routes/viewRouter.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import ProductManager from "../productManager.js";
+import { Server } from "socket.io";
+
+const viewRouter = Router();
+const productM = new ProductManager("./products.json");
+const socketServer = new Server();
+
+viewRouter.get("/", async (req, res) => {
+    try {
+      const productos = await productM.getProduct();
+      res.render("home", { products: productos });
+    } catch (error) {
+      res.status(500).send({ status: "error", message: "Error al obtener los productos" });
+    }
+  });
+
+  viewRouter.get("/realtimeproducts", async (req, res) => {
+    try {
+      const products = await productM.getProduct();
+      res.render("realTimeProducts", { products });
+      
+      socketServer.emit("productsUpdated", products);
+    } catch (error) {
+      res.status(500).send({ status: "error", message: "Error al obtener los productos" });
+    }
+  });
+
+export default viewRouter;
